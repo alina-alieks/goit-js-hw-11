919ab88eed4adf41ef4d637dfb4320bdfa72c3bc
alina-alieks <145293161+alina-alieks@users.noreply.github.com>
2024-02-27 20:08:02 +0200

    complete

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,86 +1,48 @@
-//функція для HTTP запитів
-
+//Імпорт iziToast для відображення повідомлення
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 import iconError from "../img/icon-error.svg";
 
+//Використання iziToast
+const showErrorMessage = () => iziToast.error({
+    message: 'Sorry, there are no images matching your search query. Please try again!',
+    iconUrl: iconError,
+    position: "topRight",
+    backgroundColor: "#ef4040",
+    theme: "dark",
+    transitionIn: "fadeInRight",
+})
+
+
 const form = document.querySelector(".form")
-
-
-console.log(form.imput.image.value)
-// form.addEventListener("submit", getImagesFromPixabay)
+const loader = document.querySelector(".loader")
 
 const baseUrl = "https://pixabay.com/api/";
 const key = "42527705-4e95d3f46fcc8571248d3eb24";
 
-function getImagesFromPixabay(event) {
-  event.preventDefault();
-  const input = event.currentTarget.elements.image.value.trim();
-  const inputSearch = input.split(" ").join("+")
-  const url = `${baseUrl}?key=${key}&q=${inputSearch}&image_type=photo&orientation=horizontal&safesearch=true`
-  getDataImages(url);
-  form.reset()
+//Формування URL для HTTP запита
+function getApiRequest(value) {
+  const url = `${baseUrl}?key=${key}&q=${value}&image_type=photo&orientation=horizontal&safesearch=true`;
   return url
 }
+//Функція додавання та видалення класу "is-hidden" для видимості завантажувача
+const showLoader = () => loader.classList.toggle("is-hidden");
 
-function getDataImages(url) {
-  const showErrorMessage = () => iziToast.error({
-          message: 'Sorry, there are no images matching your search query. Please try again!',
-          iconUrl: iconError,
-          position: "topRight",
-          backgroundColor: "#ef4040",
-          theme: "dark",
-          transitionIn: "fadeInRight",
-  })
-  
-  fetch(url)
+//Формування запита на сервер з перевіркою на заповнення поля вводу інпута та отримання відповіді (проміса)
+function getResponse(url) {
+
+  if (form.image.value.trim()) {
+    showLoader();
+    return fetch(url)
       .then(response => {
-        if (!response.ok) {
-          throw new Error("Error")
-        }
-          return response.json()
-      })
-      .then((data) => { 
-        if (data.hits.length !== 0) {
-          console.log(data.hits)
-          getGallery(data)
-        } else (
-          showErrorMessage()
-        )
+        return response.json();
       })
       .catch(error => error.message)
+  } else {
+    showErrorMessage()
+  }
+  
 }
 
-const gallery = document.querySelector(".gallery");
-
-function getGallery(data) {
-  console.log(data)
-  let createGallery = "";
-  data.hits.map(({ webformatURL, tags, likes, views, comments, downloads }) => {
-          return createGallery += `<li class="gallery-item">
-          <img class="gallery-item-images" src="${webformatURL}" alt="${tags}" />
-          <ul class="gallery-item-info">
-            <li class="item-info">
-              <p class="item-info-name">Likes</p>
-              <p class="item-info-value">${likes}</p>
-            </li>
-            <li class="item-info">
-              <p class="item-info-name">Views</p>
-              <p class="item-info-value">${views}</p>
-            </li>
-            <li class="item-info">
-              <p class="item-info-name">Comments</p>
-              <p class="item-info-value">${comments}</p>
-            </li>
-            <li class="item-info">
-              <p class="item-info-name">Downloads</p>
-              <p class="item-info-value">${downloads}</p>
-            </li>
-          </ul>
-        </li>`
-    }).join("");
-    gallery.innerHTML = createGallery;
-}
-
-
-export {getImagesFromPixabay}
+//Експорт функцій 
+export {getApiRequest, getResponse, showErrorMessage, showLoader}
--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,9 +1,88 @@
-// функція для відображення елементів інтерфейсу
+// Імпорт бібліотеки SimpleLightbox - відображення модалки фото
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+//Імпорт функцій з "./pixabay-api"
+import { getApiRequest, getResponse, showErrorMessage, showLoader } from "./pixabay-api";
 
-import { getImagesFromPixabay } from "./pixabay-api";
+
+const form = document.querySelector(".form");
+const gallery = document.querySelector(".gallery");
 
 
-const form = document.querySelector(".form")
-
+//Функція до слухача події до кнопки - відправка запиту та відповідь
+function getImagesFromPixabay(event) {
+    event.preventDefault();
+    gallery.innerHTML = "";
+    const input = event.currentTarget.elements.image.value.trim();
+    const inputSearch = input.split(" ").join("+");
+    const url = getApiRequest(inputSearch);
+    const datas = getResponse(url);
+    // console.log(datas)
+    if (datas) { 
+        datas.then(data => {
+            showLoader();
+            if (data.hits.length !== 0) {
+                // console.log(data.hits)
+                markup(data);
+            } else (
+                showErrorMessage()
+            )
+        })}
+    form.reset()
+}
 form.addEventListener("submit", getImagesFromPixabay)
+
+
+
+
+
+//Вікористання бібліотеки
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250 } );
+
+//Додавання HTML коду списку галереї
+function markup(data) {
+//   console.log(data)
+let createGallery = "";
+data.hits.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
+    return createGallery += `<li class="gallery-item">
+        <a href="${largeImageURL}"><img class="gallery-item-images" src="${webformatURL}" alt="${tags}" /></a>
+        <ul class="gallery-item-info">
+            <li class="item-info">
+                <p class="item-info-name">Likes</p>
+                <p class="item-info-value">${likes}</p>
+            </li>
+            <li class="item-info">
+                <p class="item-info-name">Views</p>
+                <p class="item-info-value">${views}</p>
+            </li>
+            <li class="item-info">
+                <p class="item-info-name">Comments</p>
+                <p class="item-info-value">${comments}</p>
+            </li>
+            <li class="item-info">
+                <p class="item-info-name">Downloads</p>
+                <p class="item-info-value">${downloads}</p>
+            </li>
+            </ul>
+    </li>`
+    }).join("");
+    gallery.innerHTML = createGallery;
+    //Оновлення метод refresh() для даних Ajax Calls або після DOM-маніпуляцій 
+    lightbox.refresh();
+    //Стилі бібліотеки simplelightbox
+    lightbox.on('shown.simplelightbox', function () {
+    const overlay = document.querySelector(".sl-overlay");
+    const btnClose = document.querySelector(".sl-close ");
+    const counter = document.querySelector(".sl-counter");
+    const btnArrow = document.querySelectorAll(".sl-navigation button");
+    const overlayImage = document.querySelector(".sl-caption");
+
+    overlay.style.backgroundColor = "rgb(46, 47, 66)";
+    btnClose.style.color = "#ffffff";
+    btnClose.style.fontSize = "2rem";
+    counter.style.color = "#ffffff";
+    btnArrow.forEach(item => item.style.color = "#ffffff");
+    overlayImage.style.backgroundColor = "rgba(46, 47, 66, 0.80)";
+    });
+}
 
