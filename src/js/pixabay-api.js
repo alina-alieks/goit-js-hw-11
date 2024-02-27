d3bb892877c0851b2a07898fb07c248acd4dfd18
alina-alieks <145293161+alina-alieks@users.noreply.github.com>
2024-02-28 00:36:39 +0200

    update

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,50 +1,12 @@
-//Імпорт iziToast для відображення повідомлення
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
-import iconError from "../img/icon-error.svg";
-
-//Використання iziToast
-const showErrorMessage = () => iziToast.error({
-    message: 'Sorry, there are no images matching your search query. Please try again!',
-    iconUrl: iconError,
-    position: "topRight",
-    backgroundColor: "#ef4040",
-    theme: "dark",
-    transitionIn: "fadeInRight",
-})
-
-
-const form = document.querySelector(".form")
-const loader = document.querySelector(".loader")
 
 const baseUrl = "https://pixabay.com/api/";
 const key = "42527705-4e95d3f46fcc8571248d3eb24";
 
-//Формування URL для HTTP запита
-function getApiRequest(value) {
+//Формування HTTP запита на сервер для отримання відповіді (проміса)
+function getImagesService(value) {
   const url = `${baseUrl}?key=${key}&q=${value}&image_type=photo&orientation=horizontal&safesearch=true`;
-  return url
-}
-//Функція додавання та видалення класу "is-hidden" для видимості завантажувача
-const showLoader = () => loader.classList.toggle("is-hidden");
-
-//Формування запита на сервер з перевіркою на заповнення поля вводу інпута та отримання відповіді (проміса)
-function getResponse(url) {
-
-  if (form.image.value.trim()) {
-    showLoader();
-    return fetch(url)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`Error request ${response.status}`)
-        }
-        return response.json();
-      })
-      } else {
-    showErrorMessage()
-  }
-  
+  return fetch(url);
 }
 
 //Експорт функцій 
-export {getApiRequest, getResponse, showErrorMessage, showLoader}
+export { getImagesService }
--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -2,11 +2,28 @@
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 //Імпорт функцій з "./pixabay-api"
-import { getApiRequest, getResponse, showErrorMessage, showLoader } from "./pixabay-api";
+import { getImagesService } from "./pixabay-api";
+//Імпорт iziToast для відображення повідомлення
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+import iconError from "../img/icon-error.svg";
 
+//Використання iziToast
+const showErrorMessage = () => iziToast.error({
+    message: 'Sorry, there are no images matching your search query. Please try again!',
+    iconUrl: iconError,
+    position: "topRight",
+    backgroundColor: "#ef4040",
+    theme: "dark",
+    transitionIn: "fadeInRight",
+})
 
 const form = document.querySelector(".form");
 const gallery = document.querySelector(".gallery");
+const loader = document.querySelector(".loader")
+
+//Функція додавання та видалення класу "is-hidden" для видимості завантажувача
+const showLoader = () => loader.classList.toggle("is-hidden");
 
 
 //Функція до слухача події до кнопки - відправка запиту та відповідь
@@ -15,27 +32,32 @@ function getImagesFromPixabay(event) {
     gallery.innerHTML = "";
     const input = event.currentTarget.elements.image.value.trim();
     const inputSearch = input.split(" ").join("+");
-    const url = getApiRequest(inputSearch);
-    const datas = getResponse(url);
-    // console.log(datas)
-    if (datas) { 
-        datas
+    
+    if (input) {
+        showLoader();
+        getImagesService(inputSearch)
+            .then(response => {
+                if (!response.ok) {
+                throw new Error(`Error request ${response.status}`)
+                }
+                return response.json();
+                })
             .then(data => {
-            showLoader();
-            if (data.hits.length !== 0) {
-                // console.log(data.hits)
-                markup(data);
-            } else (
-                showErrorMessage()
-                )
+                showLoader();
+                if (data.hits.length !== 0) {
+                    // console.log(data.hits)
+                    markup(data);
+                    form.reset()
+                } else {
+                    showErrorMessage()
+                    }
             })
-            .catch(() => {
-                // console.log(error);
+            .catch(error => {
+                console.log(error);
                 showLoader()
                 showErrorMessage();
             })
         }
-    form.reset()
 }
 form.addEventListener("submit", getImagesFromPixabay)
 
